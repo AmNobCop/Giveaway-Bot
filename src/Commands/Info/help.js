8673d6881c1b47930ae6258565c8fb3103bcf40b
AmNobCop <96837273+AmNobCop@users.noreply.github.com>
2023-01-26 20:03:06 -0500

    Add files via upload

---
+++ src/Commands/Info/help.js
@@ -0,0 +1,24 @@
+const { Client, ChatInputCommandInteraction, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'help',
+    description: '🗒️ Help menu',
+    /**
+     * 
+     * @param {ChatInputCommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    execute(interaction, client) {
+        const embed = new EmbedBuilder()
+        .setColor('DarkRed')
+        .setThumbnail('https://images.emojiterra.com/twitter/v13.1/512px/1f4cb.png')
+        .setTitle('Help Menu')
+        .setDescription(`> You can find all the commands here.\n> In total there are **\`8\`** commands.`)
+        .addFields([
+            { name: 'ℹ️ Info', value: '**[2]** `help`, `ping`' },
+            { name: '🎉 Giveaways', value: '**[6]** `giveaway start`, `giveaway pause`, `giveaway unpause`, `giveaway end`, `giveaway reroll`, `giveaway delete`'}
+        ]);
+        
+        return interaction.reply({ embeds: [embed]})
+    }
+}
---
+++ src/config.js
@@ -0,0 +1,45 @@
+module.exports = {
+    app : {
+        // Discord Bot Token from https://discord.com/developers/
+        token: 'XXX',
+        // Discord Bot Status
+        playing: 'giveaways 🎉',
+        // Database URL from https://mongodb.com/
+        mongodb: 'XXX',
+        // If you only want to have slash commands for one guild, leave false.
+        globalCommands: false,
+        // If you chose false above, put the guild id where you want to have slash commands.
+        slashGuild: 'XXX'
+    },
+
+    giveawayManager : {
+        //Private Message Information.
+        //If you set false, the bot will not send private message information to members who join the giveaway, for example.
+        privateMessageInformation: true,
+        // Role ID which will be able to create giveaways
+        giveawaysManagerRole: 'XXX',
+        // When a giveaway is created the bot pings everyone (true or false)
+        everyoneMention: false,
+        // You can choose a custom reaction
+        reaction: '🎉'
+    },
+    
+    messages: {
+        giveaway: '🎉 **Giveaway**',
+        giveawayEnded: '🎉 **Giveaway Ended**',
+        title: 'Prize: {this.prize}',
+        drawing: 'The giveaway ends in: {timestamp}',
+        dropMessage: 'Be the first, and react to 🎉!',
+        inviteToParticipate: 'React with 🎉 to enter the giveaway!',
+        winMessage: 'Congratulations, {winners}! You won **{this.prize}**!',
+        embedFooter: '{this.winnerCount} winner(s)',
+        noWinner: 'Giveaway cancelled, no valid participations.',
+        hostedBy: 'Hosted by: {this.hostedBy}',
+        winners: 'Winner(s):',
+        endedAt: 'Ended at',
+        paused: '⚠️ **This giveaway is paused!**',
+        infiniteDurationText: '`NEVER`',
+        congrat: 'New winner(s): {winners}! Congratulations, your prize is **{this.prize}**!',
+        error: 'Reroll cancelled, no valid participations.'
+    }
+}
